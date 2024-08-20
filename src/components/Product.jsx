dc8517b3966cb359820b5ea5a2d952ab69f992ee
ArpitRathi1 <rathi844@gmail.com>
2024-08-20 09:23:27 +0530

    update

---
+++ src/components/Product.jsx
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from "react-redux"
+import { add, remove } from "../redux/slices/cartSlice";
+import { toast } from "react-hot-toast";
+
+function Product({post}){
+
+    const cart = useSelector((state) => state.cart)
+    const dispatch = useDispatch()
+
+    function addToCart(){
+        dispatch(add(post));
+        toast.success("Item added to cart")
+    }
+
+    function removeFromCart(){
+        dispatch(remove(post.id))
+        toast.error("Irem removed from cart")
+    }
+
+    return(
+        <div className="flex flex-col items-center justify-between gap-3 p-4 mt-10 ml-5 rounded-xl
+        hover:scale-110 transition-all duration-200 ease-in hover:shadow-custom">
+            <div>
+                <p className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{post.title.split(" ").slice(0,3).join(" ") + "..."}</p>
+            </div>
+            <div>
+                <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ") + "..."}</p>
+            </div>
+            <div className="h-[180px]">
+                <img className="h-full w-full" src={post.image}></img>
+            </div>
+
+            <div className="flex gap-12 justify-center items-center w-full mt-5">
+                <div >
+                    <p className="text-green-600 font-semibold">${post.price}</p>
+                </div>
+                {
+                    cart.some((p) => p.id == post.id) ? 
+                    <button 
+                    className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] p-1 px-3 uppercase
+                    hover:bg-gray-700 hover:text-white transition-all duration-200 ease-in" onClick={removeFromCart}>Remove Item</button> :
+                    <button 
+                    className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] p-1 px-3 uppercase
+                     hover:bg-gray-700 hover:text-white transition-all duration-200 ease-in" onClick={addToCart}>Add To Cart</button>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Product
