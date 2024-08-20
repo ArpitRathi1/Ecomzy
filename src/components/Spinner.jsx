b1336253b17bd8c4828b8315513adb81a351ecb8
ArpitRathi1 <rathi844@gmail.com>
2024-08-20 09:24:21 +0530

    update

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import "./Spinner.css"
+
+function Spinner(){
+    return (
+        <div className="spinner-container">
+            <div className="Spinner"></div>
+        </div>
+    )
+}
+
+export default Spinner
